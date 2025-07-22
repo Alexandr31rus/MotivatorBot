3c46ead97c1cd91c8596b62e356efd108e30dc98
Macbook Pro <macbookpro@MacBook-Pro-Macbook.local>
2025-07-22 22:22:21 +0300

    modified:   app/database/models.py

--- app/database/models.py
+++ app/database/models.py
@@ -2,10 +2,10 @@ from sqlalchemy import BigInteger, String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 
-engine = create_async_engine(url='sqlite+aiosqlite:///database.db',
-                             echo=True)
+engine = create_async_engine(url="sqlite+aiosqlite:///database.db", echo=True)
 
 async_session = async_sessionmaker(engine)
+
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
@@ -13,10 +13,23 @@ class Base(AsyncAttrs, DeclarativeBase):
 
 # Создание класса User, который является дочерним к классу Base
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     tg_id = mapped_column(BigInteger)
     name: Mapped[str] = mapped_column(String(25), nullable=True)
 
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(25))
+
+class Card(Base):
+    __tablename__ = 'cards'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
 
