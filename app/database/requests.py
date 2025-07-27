b4a5a80c404a4c5ace4895dadfbda5eb72373a31
Александр <aleksandr@Mac-mini-Aleksandr.local>
2025-07-28 00:31:19 +0300

    изменено:      app/database/requests.py 	изменено:      app/keyboards.py 	изменено:      app/user.py

--- app/database/requests.py
+++ app/database/requests.py
@@ -1,6 +1,7 @@
 from app.database.models import async_session, User, Category, Card
 from sqlalchemy import select, update
-
+import random
+from sqlalchemy.sql.expression import func
 
 # Функция создания нового пользователя
 async def set_user(tg_id):
@@ -46,3 +47,11 @@ async def get_cards_by_category(category_id):
 async def get_card(card_id):
     async with async_session() as session:
         return await session.scalar(select(Card).where(Card.id == card_id))
+
+async def get_cards_by():
+    async with async_session() as session:
+        return await session.scalars(select(Card))
+
+async def random_cards(card_id: int):
+    async with async_session() as session:
+        return await session.scalars(select(Card).where(Card.id == card_id))
